71ad32242197b7f0e8d334a29abdf1ffac6f9d45
Toon Bertier <toonbertier@gmail.com>
2015-05-30 15:29:17 +0200

    check if user has seen tutorial screen closes #8

--- Badget/AppDelegate.swift
+++ Badget/AppDelegate.swift
@@ -18,6 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         // Override point for customization after application launch.
+        
+        if(NSUserDefaults.standardUserDefaults().boolForKey("tutorialSeen") == true) {
+            println("tutorial seen")
+        } else {
+            NSUserDefaults.standardUserDefaults().setBool(true, forKey: "tutorialSeen")
+            println("tutorial not seen")
+        }
+        
         self.window!.backgroundColor = UIColor.whiteColor()
         self.window!.makeKeyAndVisible()
         return true
